aa9f23e6aba9a14a03686d6b62cee887fceaba0d
dnzambuli <damunzandm@outlook.com>
2024-03-02 12:05:19 +0300

    confirming gitignore

--- Front End/src/main.py
+++ Front End/src/main.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 
 # create route
 # routes are endpoints for a server
-@app.route("/") # constructor to a target root
+@app.route("/")
 def home():
     return "Home"
+
 
 # running the app
 if __name__ == '__main__':
