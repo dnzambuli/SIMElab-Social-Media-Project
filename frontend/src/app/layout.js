9a65d14bd3965f2fa6d54c8d714c131f2b9aa800
dnzambuli <damunzandm@outlook.com>
2024-03-03 15:03:58 +0300

    more components

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,10 +1,12 @@
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
+  title: 'SIME Social Listening',
   description: 'The best blog app!',
 }
 
@@ -12,8 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='container'>
-          <div className='wrapper'>{children}</div>
+        <div className="container">
+          <div className="wrapper">
+            <Navbar/>
+            {children}
+            <Footer/>
+          </div>
         </div>
       </body>
     </html>
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,13 +1,10 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+
 
 export default function Home() {
   return(
   <div>
-    <Navbar/>
-    <Footer/>
   </div>
     );
 }
