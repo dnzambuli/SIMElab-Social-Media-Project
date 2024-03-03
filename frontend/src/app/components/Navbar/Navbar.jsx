1f859320b4f0286d06400309b69cbbc67ac51487
dnzambuli <damunzandm@outlook.com>
2024-03-03 21:37:33 +0300

    styled the base for the navbar

--- frontend/src/app/components/Navbar/Navbar.jsx
+++ frontend/src/app/components/Navbar/Navbar.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import styles from "./navbar.module.css";
+import Image from 'next/image';
+import Link from 'next/link';
+import ThemeToggle from '../themeToggle/ThemeToggle';
 
 const Navbar = () =>{
     return(
-        <div className = {styles.container}>Navbar</div>
+        <div className = {styles.container}>
+            <div className={styles.Logo}>
+                <Image src="/SIME-lab-logo.png" alt='SIME Lab' width={80} height={42}/>
+            </div>
+
+            <div className={styles.home}>
+                <Link href='/'>Home</Link>
+            </div>
+
+            <div className={styles.links}>
+                <Link href='/'>Emerging Stories</Link>
+                <Link href='/'>Entertainment</Link>
+                <Link href='/'>Contact</Link>
+                <ThemeToggle/>
+
+            </div>
+
+        </div>
     );
 }
 
