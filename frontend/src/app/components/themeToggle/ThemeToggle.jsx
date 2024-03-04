47fceeb4b43f95106d76b7e69292df80687cfa41
dnzambuli <damunzandm@outlook.com>
2024-03-04 10:43:16 +0300

    added the color codes for dark theme and the toggle function

--- frontend/src/app/components/themeToggle/ThemeToggle.jsx
+++ frontend/src/app/components/themeToggle/ThemeToggle.jsx
@@ -1,7 +1,11 @@
+"use client";
 import Image from "next/image";
 import styles from "./themeToggle.module.css"
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () =>{
+    const {theme} = useContext(ThemeContext);
     return(
         <div className={styles.container}>
             <Image src="/moon.png" alt="" width={14} height={14}/>
