800ba789e7a53f3545503b97f422570e5e84478f
dnzambuli <damunzandm@outlook.com>
2024-03-04 13:59:06 +0300

    added the toggle to the html

--- frontend/src/app/components/themeToggle/ThemeToggle.jsx
+++ frontend/src/app/components/themeToggle/ThemeToggle.jsx
@@ -5,9 +5,9 @@ import { useContext } from "react";
 import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () =>{
-    const {theme} = useContext(ThemeContext);
+    const {toggle,theme} = useContext(ThemeContext);
     return(
-        <div className={styles.container}>
+        <div className={styles.container} onClick={toggle}>
             <Image src="/moon.png" alt="" width={14} height={14}/>
             <div className={styles.ball}></div>
             <Image src="/sun.png" alt = "" width={14} height={14}/>
