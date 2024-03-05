75232810672cd3436be7af067b03514820cca54d
dnzambuli <damunzandm@outlook.com>
2024-03-05 10:48:26 +0300

    made the burger menu

--- frontend/src/app/components/Navbar/Navbar.jsx
+++ frontend/src/app/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import styles from "./navbar.module.css";
 import Image from 'next/image';
 import Link from 'next/link';
 import ThemeToggle from '../themeToggle/ThemeToggle';
 
 const Navbar = () =>{
+    const [open, setOpen] = useState(false);
     return(
         <div className = {styles.container}>
             <div className={styles.Logo}>
@@ -16,12 +18,26 @@ const Navbar = () =>{
             </div>
 
             <div className={styles.links}>
-                <Link href='/'>Latest</Link>
-                <Link href='/'>Entertainment</Link>
-                <Link href='/'>Contact</Link>
+                <Link href='/' className={styles.link}>Latest</Link>
+                <Link href='/' className={styles.link}>Entertainment</Link>
+                <Link href='/' className={styles.link}>Contact</Link>
                 <ThemeToggle/>
 
             </div>
+            <div className={styles.burger} onClick={() => setOpen(!open)}>
+                <div className={styles.lines}></div>
+                <div className={styles.lines}></div>
+                <div className={styles.lines}></div>
+
+            </div>
+            {open &&(
+                <div className={styles.responsiveMenu}>
+                    <Link href='/'>Homepage</Link>
+                    <Link href='/' >Latest</Link>
+                    <Link href='/' >Entertainment</Link>
+                    <Link href='/' >Contact</Link>
+                </div>
+            )}
 
         </div>
     );
